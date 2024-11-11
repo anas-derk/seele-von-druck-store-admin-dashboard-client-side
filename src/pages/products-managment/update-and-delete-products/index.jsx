1f36bce659a81d318511d7018c8854b91133ecbd
anas derk <anas.derk2020@gmail.com>
2024-11-11 15:23:20 +0300

    Fix

--- src/pages/products-managment/update-and-delete-products/index.jsx
+++ src/pages/products-managment/update-and-delete-products/index.jsx
@@ -525,6 +525,7 @@ export default function UpdateAndDeleteProducts() {
                                     <th>Description</th>
                                     <th>Category</th>
                                     <th>Discount</th>
+                                    <th>Tax</th>
                                     <th>Image</th>
                                     <th>Processes</th>
                                 </tr>
